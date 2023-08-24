5de99156cc58c099ed001b0b699814bf64269b02
Emmet Hayes <ehayes1@alumni.berklee.edu>
2023-08-24 11:45:59 -0400

    Refactored out the front panel component to be in same structure as other plugins

--- Source/HayesReverbAudioProcessorEditor.cpp
+++ Source/HayesReverbAudioProcessorEditor.cpp
@@ -3,18 +3,61 @@
 
 HayesReverbAudioProcessorEditor::HayesReverbAudioProcessorEditor (HayesReverbAudioProcessor& p, juce::UndoManager& um)
 :   BaseAudioProcessorEditor { p }
-,   editorContent        { p, um }
+,   processor            { p }
 ,   presetBar            { p }
+,   freezeAttachment (*processor.apvts.getParameter (ParamIDs::freeze), freezeButton, &um)
+{
+    setLookAndFeel(&customLookAndFeel);
+    addAllGUIComponents();
+}
+
+HayesReverbAudioProcessorEditor::~HayesReverbAudioProcessorEditor()
+{
+    setLookAndFeel(nullptr);
+}
+
+void HayesReverbAudioProcessorEditor::addAllGUIComponents()
 {
     const auto ratio = static_cast<double> (defaultWidth) / defaultHeight;
     setResizable(false, true);
     getConstrainer()->setFixedAspectRatio (ratio);
     getConstrainer()->setSizeLimits(defaultWidth, defaultHeight, defaultWidth * 2, defaultHeight * 2);
     image = juce::ImageCache::getFromMemory(BinaryData::bg_file_jpg, BinaryData::bg_file_jpgSize);
+
+    sizeLabel.setText("Size", juce::dontSendNotification);
+    dampLabel.setText("Damp", juce::dontSendNotification);
+    widthLabel.setText("Width", juce::dontSendNotification);
+    mixLabel.setText("Mix", juce::dontSendNotification);
+    
+    addAndMakeVisible(sizeLabel);
+    addAndMakeVisible(dampLabel);
+    addAndMakeVisible(widthLabel);
+    addAndMakeVisible(mixLabel);
+
+    sizeDial.setSliderStyle(juce::Slider::Rotary);
+    dampDial.setSliderStyle(juce::Slider::Rotary);
+    widthDial.setSliderStyle(juce::Slider::Rotary);
+    mixDial.setSliderStyle(juce::Slider::Rotary);
+
+    sizeDial.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 70, 20);
+    dampDial.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 70, 20);
+    widthDial.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 70, 20);
+    mixDial.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 70, 20);
+
+    addAndMakeVisible (sizeDial);
+    addAndMakeVisible (dampDial);
+    addAndMakeVisible (widthDial);
+    addAndMakeVisible (mixDial);
+    addAndMakeVisible (freezeButton);
+
+    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+
+    sizeAttachment  = std::make_unique<Attachment>(processor.apvts, ParamIDs::size,  sizeDial);
+    dampAttachment  = std::make_unique<Attachment>(processor.apvts, ParamIDs::damp,  dampDial);
+    widthAttachment = std::make_unique<Attachment>(processor.apvts, ParamIDs::width, widthDial);
+    mixAttachment   = std::make_unique<Attachment>(processor.apvts, ParamIDs::mix,   mixDial);
+  
     setSize (defaultWidth, defaultHeight + 20);
-    editorContent.setSize (defaultWidth, defaultHeight);
-    addAndMakeVisible(editorContent);
-    presetBar.setLookAndFeel(&lnf->customLookAndFeel);
     addAndMakeVisible(presetBar);
 }
 
@@ -25,7 +68,25 @@ void HayesReverbAudioProcessorEditor::paint (juce::Graphics& g)
 
 void HayesReverbAudioProcessorEditor::resized()
 {
-    const auto factor = static_cast<float> (getWidth()) / defaultWidth;
-    presetBar.setBounds(0, 0, static_cast<int>(500 * factor), static_cast<int>(20 * factor));
-    editorContent.setTransform (juce::AffineTransform::scale (factor));
+    const auto scale = static_cast<float> (getWidth()) / HayesReverbAudioProcessorEditor::defaultWidth;
+
+    auto setBoundsAndApplyScaling = [&](juce::Component* component, int x, int y, int w, int h, bool isSlider = false)
+    {
+        component->setBounds(static_cast<int>(x * scale), static_cast<int>(y * scale),
+            static_cast<int>(w * scale), static_cast<int>(h * scale));
+        if (isSlider)
+            dynamic_cast<juce::Slider*>(component)->setTextBoxStyle(juce::Slider::TextBoxBelow, false, static_cast<int>(70 * scale), static_cast<int>(20 * scale));
+    };
+    
+    customLookAndFeel.setWindowScale(scale);
+    setBoundsAndApplyScaling(&presetBar, 0, 0, 500, 20);
+    setBoundsAndApplyScaling(&sizeLabel, 41, 70, 70, 20);
+    setBoundsAndApplyScaling(&sizeDial, 16, 95, 80, 80, true);
+    setBoundsAndApplyScaling(&dampLabel, 134, 70, 70, 20);
+    setBoundsAndApplyScaling(&dampDial, 114, 95, 80, 80, true);
+    setBoundsAndApplyScaling(&widthLabel, 232, 70, 70, 20);
+    setBoundsAndApplyScaling(&widthDial, 212, 95, 80, 80, true);
+    setBoundsAndApplyScaling(&mixLabel, 335, 70, 70, 20);
+    setBoundsAndApplyScaling(&mixDial, 310, 95, 80, 80, true);
+    setBoundsAndApplyScaling(&freezeButton, 410, 110, 68, 32);
 }
--- Source/HayesReverbAudioProcessorEditor.h
+++ Source/HayesReverbAudioProcessorEditor.h
@@ -2,38 +2,47 @@
 
 #include <JuceHeader.h>
 #include "HayesReverbAudioProcessor.h"
-#include "HayesReverbFrontPanelComponent.h"
 #include "../../Common/BaseAudioProcessorEditor.h"
 #include "../../Common/CustomLookAndFeel.h"
 #include "../../Common/PresetBar.h"
+#include "../../Common/PercentSlider.h"
+#include "GUI/FreezeButton.h"
+#include "ParamIDs.h"
 
 class HayesReverbAudioProcessorEditor  : public BaseAudioProcessorEditor
 {
 public:
     HayesReverbAudioProcessorEditor (HayesReverbAudioProcessor&, 
                                       juce::UndoManager& um);
+    ~HayesReverbAudioProcessorEditor();
 
     void paint (juce::Graphics&) override;
     void resized() override;
 
-    static constexpr int defaultWidth  { 500 };
-    static constexpr int defaultHeight { 250 };
-
 private:
-    HayesReverbFrontPanelComponent editorContent;
+    void addAllGUIComponents();
+    
+    CustomLookAndFeel customLookAndFeel;
+
+    HayesReverbAudioProcessor& processor;
+  
     PresetBar presetBar;
+    
     juce::Image image;
 
+    PercentSlider sizeDial, dampDial, widthDial, mixDial;
+    FreezeButton freezeButton;
 
-    struct SharedLnf
-    {
-        SharedLnf()  { juce::LookAndFeel::setDefaultLookAndFeel (&customLookAndFeel); }
-        ~SharedLnf() { juce::LookAndFeel::setDefaultLookAndFeel (nullptr); }
+    juce::Label sizeLabel, dampLabel, widthLabel, mixLabel;
 
-        CustomLookAndFeel customLookAndFeel;
-    };
-
-    juce::SharedResourcePointer<SharedLnf> lnf;
-
+    juce::ButtonParameterAttachment freezeAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> sizeAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> dampAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> widthAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> mixAttachment;
+  
+    static constexpr int defaultWidth  { 500 };
+    static constexpr int defaultHeight { 250 };
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (HayesReverbAudioProcessorEditor)
 };
